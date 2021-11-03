3bdd0204233a513dc9d524c0081f518c34dde23b
Vaishnavitopper <rvaish1234@gmail.com>
2021-11-04 01:24:15 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,27 +9,37 @@ function App() {
       <div className='nurse'></div>
       <div className='box'>
         <form>
-          <h1 className='head1'>Hospital Login</h1>
+        <h1 className='head1'>Hospital Login</h1>
+        <input className="input1" type="text" required placeholder="Enter Username Here"></input>
+          <br/>
+          <input className="input" type="password" required placeholder="Enter Password Here" ></input>
+          <br/>
+          <label class="container">
+          <input type="radio"/>Remember Me <span class="checkmark"></span> </label>
+          <br/>
+          <br/>
+          <br/>
+          <button className="log" onsubmit="required()">Log In</button>     
+          <br/>
+          <br/>
+          <h4 id="register">Not Registered Yet? Contact Admin</h4> 
+          <br/>
+          {/* <h1 className='head1'>Hospital Login</h1>
           <br></br>
           <lable for="email"><b></b></lable>
-          <input type="text" placeholder='Enter Username Here' name ="email" required></input>
+          <input type="text" id="user" placeholder='Enter Username Here' name ="email" required></input>
           <label for="psw"><b></b></label>
           <br></br>
-          <input type="password" placeholder="Enter Password Here" name="psw" required></input>
-          <br></br><div id="remme">
-          <input type="checkbox" value="lsRememberMe" id="rememberMe" text-align="left"/>  <label for="rememberMe" >Remember me</label></div>
+          <input type="password" id="pass" placeholder="Enter Password Here" name="psw" required></input>
+          <br></br>
+          <input type="checkbox" value="lsRememberMe" id="rememberMe" text-align="left"/> <label for="rememberMe">Remember me</label>
           <br></br>
           <br></br>
           <button type="submit" class="btn">Login</button>
-          <h5>Not Registered Yet?Contact Admin </h5>
+          <h5>Not Registered Yet?Contact Admin</h5> */}
         </form>
       </div>
     </div>
   );
 }
 export default App;
-
-// git remote add origin2 https://github.com/1adityas/HMS.git
-// git branch -M main
-// git push -u origin2 main
-//hello
