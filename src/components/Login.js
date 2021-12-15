29de044092221cc6d9506a8d10961aecc8a0fc79
aditya <1adityapsingh@gmail.com>
2021-12-15 23:26:26 +0700

    bg src

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,14 @@
 import React from 'react'
+import bg from '../background.svg';
 import './Login.css'
 // import {ReactComponent as Back} from '../background.svg';
 function Login() {
 
     return (
-        <div className="grid-container">
+        <   div className="grid-container" >
             <div className="grid-item1" >
                 
-         
+            <img src ={bg}  height={"700rem"} width={"auto"} ></img>
        </div>
        </div>
     )
