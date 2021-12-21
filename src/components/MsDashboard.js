3d0afe923b0b947f9073794c552e6912d197affb
Devang Mathur <masterdevang@gmail.com>
2021-12-21 14:58:02 +0530

    card

--- src/components/MsDashboard.js
+++ src/components/MsDashboard.js
@@ -16,45 +16,47 @@ function MsDashboard() {
     var [b1Color, setb1Color] = useState('#EEEEEE');
     var [b2Color, setb2Color] = useState('#506F90');
     return (
-    //using body tag to get bg color
-        <body>
-            
+        //using body tag to get bg color
+        <body className="page-container">
+
             <nav>
-            <Header/> 
+                <Header />
             </nav>
-            
-            <div>
+
+            <div className ="content-wrap">
                 <div>
-            <p style={{border:'2px dashed', textAlign:'center',fontWeight:'bold', fontSize:'28px', color:'#253143'}}>Welcome To Malad Jumbo COVID Hospital</p>
+                    <p style={{ border: '2px dashed', textAlign: 'center', fontWeight: 'bold', fontSize: '28px', color: '#253143' }}>Welcome To Malad Jumbo COVID Hospital</p>
 
-<Button variant="contained" style={{marginRight:'60px',marginLeft:'38vw',borderRadius: '12px',
-    color:`${b1Color}`,backgroundColor:`${b2Color}`,boxShadow:'#00000029'}} 
-    sx={{ boxShadow: '5px 5px 15px #00000029;' }} onClick={()=>{if(b1Color==='#EEEEEE'){setb2Color('#EEEEEE');setb1Color('#506F90')}}} >New Admission</Button>
+                    <Button variant="contained" style={{
+                        marginRight: '60px', marginLeft: '38vw', borderRadius: '12px',
+                        color: `${b1Color}`, backgroundColor: `${b2Color}`, boxShadow: '#00000029'
+                    }}
+                        sx={{ boxShadow: '5px 5px 15px #00000029;' }} onClick={() => { if (b1Color === '#EEEEEE') { setb2Color('#EEEEEE'); setb1Color('#506F90') } }} >New Admission</Button>
 
-<Button variant="contained" style={{color:`${b2Color}`,backgroundColor:`${b1Color}`,borderRadius: '12px',}} sx={{ boxShadow: '5px 5px 15px #00000029;' }} onClick={()=>{if(b2Color==='#EEEEEE'){setb1Color('#EEEEEE');setb2Color('#506F90')}}}>Ward Details</Button>
-{/* hooks to be used for buttons */}
-                 </div>
+                    <Button variant="contained" style={{ color: `${b2Color}`, backgroundColor: `${b1Color}`, borderRadius: '12px', }} sx={{ boxShadow: '5px 5px 15px #00000029;' }} onClick={() => { if (b2Color === '#EEEEEE') { setb1Color('#EEEEEE'); setb2Color('#506F90') } }}>Ward Details</Button>
+                    {/* hooks to be used for buttons */}
+                </div>
 
-            <div className=".cards">
-              <div className= "card"></div>
-              <div className= "card"></div>
-              <div className= "card"></div>
-              <div className= "card"></div>
-              <div className= "card"></div>
-              
+                <div className="cards">
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
+                    <div className="card"></div>
 
-           </div>
+
+                </div>
             </div>
 
 
-            
 
-            <div>
-            <MainFooter />
+
+            <div className= "footer">
+                <MainFooter />
             </div>
-             
-            
+
+
         </body>
-        )
+    )
 }
 export default MsDashboard;
