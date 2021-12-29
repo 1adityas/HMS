7e0e1b5928fdfdba4c6018968ce8de9de793fc6f
aditya <1adityapsingh@gmail.com>
2021-12-29 20:26:06 +0700

    button of new admission set  and images downloded

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div>
        {/* <NavBar /> */}
-      <MsDashboard />
+      {/* <MsDashboard /> */}
       {/* < NewAdmission/> */}
       {/* <Login/> */}
       <NewAdmission/>
