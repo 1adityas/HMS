0517c4ed041a9500ad218ab3f80264fc39ead946
Vaishnavi <rvaish1234@gmail.com>
2022-01-03 01:09:11 +0530

    IA tables set

--- src/components/InitialAssessment.js
+++ src/components/InitialAssessment.js
@@ -97,8 +97,10 @@ function InitialAssessment() {
 
                     </div>
                 </form>
-                <div>
-                    <p style={{'background-color': "#6F88A2",textAlign:"center",'border-radius': "15px 15px 0px 0px"}}>Vitals</p>
+                <div style={{ 'margin': "40px 70px 40px 70px", 'width': "1780px" }}>
+                    <p style={{ 'padding-top': "15px", 'font-size': '22px', 'background-color': "#6F88A2", 'text-align': "center", 'height': "60px", 'color': "#EEEEEE", 'border-radius': "15px 15px 0px 0px" }}> Vitals <Button style={{
+                        'background-color': "#EEEEEE", 'color': "#6F88A2", 'float': "right", 'border-radius': "10px", 'margin': "11px 60px 11px 0px"
+                    }}><span style={{ 'font -size': '20px' }}>+ Add</span></Button></p>
                     <Vitals/>
                 </div>
                 
@@ -108,19 +110,34 @@ function InitialAssessment() {
                     }}><span style={{ 'font -size': '20px' }}>+ Add</span></Button></p>
                     <HistoryHabitsDataGrid />
                 </div>
-                <div>
+                <div style={{ 'margin': "40px 70px 40px 70px", 'width': "1780px" }}>
+                <p style={{ 'padding-top': "15px", 'font-size': '22px', 'background-color': "#6F88A2", 'text-align': "center", 'height': "60px", 'color': "#EEEEEE", 'border-radius': "15px 15px 0px 0px" }}> Co-Morbidities <Button style={{
+                        'background-color': "#EEEEEE", 'color': "#6F88A2", 'float': "right", 'border-radius': "10px", 'margin': "11px 60px 11px 0px"
+                    }}><span style={{ 'font -size': '20px' }}>+ Add</span></Button></p>
                     <CoMorbidities />
                 </div>
-                <div>
+                <div style={{ 'margin': "40px 70px 40px 70px", 'width': "1780px" }}>
+                <p style={{ 'padding-top': "15px", 'font-size': '22px', 'background-color': "#6F88A2", 'text-align': "center", 'height': "60px", 'color': "#EEEEEE", 'border-radius': "15px 15px 0px 0px" }}> Allergy <Button style={{
+                        'background-color': "#EEEEEE", 'color': "#6F88A2", 'float': "right", 'border-radius': "10px", 'margin': "11px 60px 11px 0px"
+                    }}><span style={{ 'font -size': '20px' }}>+ Add</span></Button></p>
                     <Allergy />
                 </div>
-                <div>
+                <div style={{ 'margin': "40px 70px 40px 70px", 'width': "1780px" }}>
+                <p style={{ 'padding-top': "15px", 'font-size': '22px', 'background-color': "#6F88A2", 'text-align': "center", 'height': "60px", 'color': "#EEEEEE", 'border-radius': "15px 15px 0px 0px" }}>Surgery <Button style={{
+                        'background-color': "#EEEEEE", 'color': "#6F88A2", 'float': "right", 'border-radius': "10px", 'margin': "11px 60px 11px 0px"
+                    }}><span style={{ 'font -size': '20px' }}>+ Add</span></Button></p>
                     <Surgery/>
                 </div>
-                <div>
+                <div style={{ 'margin': "40px 70px 40px 70px", 'width': "1780px" }}>
+                <p style={{ 'padding-top': "15px", 'font-size': '22px', 'background-color': "#6F88A2", 'text-align': "center", 'height': "60px", 'color': "#EEEEEE", 'border-radius': "15px 15px 0px 0px" }}> Symptoms <Button style={{
+                        'background-color': "#EEEEEE", 'color': "#6F88A2", 'float': "right", 'border-radius': "10px", 'margin': "11px 60px 11px 0px"
+                    }}><span style={{ 'font -size': '20px' }}>+ Add</span></Button></p>
                     <Symptoms />
                 </div>
-                <div>
+                <div style={{ 'margin': "40px 70px 40px 70px", 'width': "1780px" }}>
+                <p style={{ 'padding-top': "15px", 'font-size': '22px', 'background-color': "#6F88A2", 'text-align': "center", 'height': "60px", 'color': "#EEEEEE", 'border-radius': "15px 15px 0px 0px" }}> Covid Status <Button style={{
+                        'background-color': "#EEEEEE", 'color': "#6F88A2", 'float': "right", 'border-radius': "10px", 'margin': "11px 60px 11px 0px"
+                    }}><span style={{ 'font -size': '20px' }}>+ Add</span></Button></p>
                     <CovidStatus />
                 </div>
             </div>
